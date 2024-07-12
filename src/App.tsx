0ba70624bb3b5ae0f79395282e7c47bd98bd266b
volhaf <Volha.f@outlook.com>
2024-07-12 16:41:40 +0200

    start task counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
 export default App;
+
+
+type CounterPropsType = {
+
+}
+
+function Counter ({}:CounterPropsType): any {
+  return (
+      <div>
+        <h3>{}</h3>
+        {'number'}
+        <div>
+          <button>inc</button>
+          <button>reset</button>
+        </div>
+
+      </div>
+  )}
