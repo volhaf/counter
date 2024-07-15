071eff2e83abefea35ff8cadec096b0e5f788b76
volhaf <Volha.f@outlook.com>
2024-07-15 13:19:17 +0200

    add styled and background-image

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -8,29 +8,27 @@ type CounterPropsType = {
 export function Counter ({}:CounterPropsType): any {
     let [count, setCount] = useState(0);
 
-    const incClickHandler = () => {
-        if (count < 5) setCount(count + 1)
-        }
-
+        const incClickHandler = () => {
+            if (count < 5) setCount(count + 1)
+            }
         const resetClickHandler = () => {
             setCount(0)
         }
 
-
         return (
             <div className={'counter'}>
-                <div>{count}</div>
-                <div className={'button_conteiner'}>
-                    <Button
-                        onClick={incClickHandler}
-                        title={'inc'}
-                        disabled={count === 5}
-                    />
-                    <Button
-                        onClick={resetClickHandler}
-                        title={'reset'}
-                        disabled={count === 0}/>
-                </div>
-
-            </div>)
+                <div className={'number'}>{count}</div>
+                    <div className={'button_conteiner'}>
+                        <Button
+                            onClick={incClickHandler}
+                            title={'inc'}
+                            disabled={count === 5}
+                        />
+                        <Button
+                            onClick={resetClickHandler}
+                            title={'reset'}
+                            disabled={count === 0}/>
+                    </div>
+            </div>
+        )
     }
