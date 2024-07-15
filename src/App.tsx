45b8898a63d997e8fa0fd46e97adc50acb447f3e
volhaf <Volha.f@outlook.com>
2024-07-15 12:09:32 +0200

    refactoring button disabled

--- src/App.tsx
+++ src/App.tsx
@@ -18,37 +18,41 @@ type CounterPropsType = {
 }
 
 function Counter ({}:CounterPropsType): any {
-    let[count, setCount] = useState(0);
-    let[on, setOn] = useState(false);
+    let [count, setCount] = useState(0);
+    let [on, setOn] = useState(false);
 
     const incClickHandler = () => {
-        if (count <5 ) {
-            setCount (count +1)
+        if (count < 5) {
+            setCount(count + 1)
             setOn(true)
         }
+    }
+    const resetClickHandler = () => {
+        setCount(0)
+        setOn(false)
     }
 
 
 
     return (
-      <div className={'counter'}>
-        <div>{count}</div>
-        <div className={'button_conteiner'}>
-          <Button
-              onClick={incClickHandler}
-              title={'inc'}
-              disabled={count===5}
-          />
-            <Button
-                onClick={()=>{}}
-                title={'reset'}
-                disabled={count===0}/>
-        </div>
+        <div className={'counter'}>
+            <div>{count}</div>
+            <div className={'button_conteiner'}>
+                <Button
+                    onClick={incClickHandler}
+                    title={'inc'}
+                    disabled={count === 5}
+                />
+                <Button
+                    onClick={resetClickHandler}
+                    title={'reset'}
+                    disabled={count === 0}/>
+            </div>
 
-      </div>
-  )}
+        </div>)
 
 
+}
 
 
 type ButtonPropsType = {
@@ -60,10 +64,9 @@ function Button ({onClick, title, disabled}:ButtonPropsType): any {
     return (
         <div>
             <button
-                onClick={onClick}>
-                {disabled}
-                {title}
-            </button>
+                onClick={onClick}
+                disabled = {disabled}
+            >{title}</button>
         </div>
     )
 }
