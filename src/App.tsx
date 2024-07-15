089768cc1bb6d397802a7789ebee07fdc215ef74
volhaf <Volha.f@outlook.com>
2024-07-15 12:54:16 +0200

    refactoring Counter and new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,21 @@
-import React, {useState} from 'react';
 
 import './App.css';
-import {Button} from "./components/Button";
+import {Counter} from "./components/Counter";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Counter/>
     </div>
-  );
+  )
 }
 
 export default App;
 
 
-type CounterPropsType = {
-
-}
-
-function Counter ({}:CounterPropsType): any {
-    let [count, setCount] = useState(0);
-    let [on, setOn] = useState(false);
-
-    const incClickHandler = () => {
-        if (count < 5) {
-            setCount(count + 1)
-            setOn(true)
-        }
-    }
-    const resetClickHandler = () => {
-        setCount(0)
-        setOn(false)
-    }
 
 
 
-    return (
-        <div className={'counter'}>
-            <div>{count}</div>
-            <div className={'button_conteiner'}>
-                <Button
-                    onClick={incClickHandler}
-                    title={'inc'}
-                    disabled={count === 5}
-                />
-                <Button
-                    onClick={resetClickHandler}
-                    title={'reset'}
-                    disabled={count === 0}/>
-            </div>
-
-        </div>)
-
-
-}
 
---
+++ src/components/Counter.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import {Button} from "./Button";
+
+type CounterPropsType = {
+
+}
+
+export function Counter ({}:CounterPropsType): any {
+    let [count, setCount] = useState(0);
+
+    const incClickHandler = () => {
+        if (count < 5) setCount(count + 1)
+        }
+
+        const resetClickHandler = () => {
+            setCount(0)
+        }
+
+
+        return (
+            <div className={'counter'}>
+                <div>{count}</div>
+                <div className={'button_conteiner'}>
+                    <Button
+                        onClick={incClickHandler}
+                        title={'inc'}
+                        disabled={count === 5}
+                    />
+                    <Button
+                        onClick={resetClickHandler}
+                        title={'reset'}
+                        disabled={count === 0}/>
+                </div>
+
+            </div>)
+    }
