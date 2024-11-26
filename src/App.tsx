25eac9c30bd49a0e365ccf4a33fc3bbff512cb4c
volhaf <Volha.f@outlook.com>
2024-11-26 16:41:05 +0100

    add new counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 
+import { useState } from 'react';
 import './App.css';
 import {Counter} from "./components/Counter";
 
 
-
 function App() {
+ const[value, setValue] =useState<number>(0)
+
+ const incHandler = () => {
+  setValue(value+1)
+ }
+
   return (
     <div className="App">
-      <Counter/>
+      {/* <Counter/> */}
+      <div className="Conteiner">
+      <h1>{value}</h1>
+      <button onClick={incHandler}> inc</button>
+      </div>
+      
     </div>
   )
 }
