77b87d0ccb7f5456d6069b8dd0bd5f0944d9b9f4
volhaf <Volha.f@outlook.com>
2024-11-28 16:01:09 +0100

    add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import {Counter} from "./components/Counter";
 import { LocalStorageHandler } from './components/LocalStorageHandler';
 
 
 function App() {
- 
 
+ 
   return (
     <div className="App">
       {/* <Counter/> */}
       <LocalStorageHandler/>
-      
+ 
       
     </div>
   )
--- src/components/LocalStorageHandler.tsx
+++ src/components/LocalStorageHandler.tsx
@@ -1,48 +1,66 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import React from "react";
+
+
+
 
 export type LocalStorageHandlerPropsType = {
 
 }
 
 
-export function LocalStorageHandler ({} :LocalStorageHandlerPropsType) {
-  const[value, setValue] =useState<number>(0)
+export function LocalStorageHandler ({} :LocalStorageHandlerPropsType): any {
+
+  const[value, setValue] = useState<number>(0)
+
+  useEffect(()=> {
+    let valueAsString = localStorage.getItem('counterKey')
+   if (valueAsString) {
+     let newValue = JSON.parse(valueAsString)
+     setValue(newValue)
+   }
+  }, [])
+
+useEffect(() => {
+    localStorage.setItem('counterKey', value.toString())
+}, [value])
+
 
   const incHandler = () => {
    setValue(value+1)
   }
  
-  const setLocalStorageHandler =() => {
- localStorage.setItem('counterKey', value.toString())
- localStorage.setItem('counterKey +1', value.toString())
-  }
+//   const setLocalStorageHandler =() => {
+//  localStorage.setItem('counterKey', value.toString())
+//  localStorage.setItem('counterKey +1', value.toString())
+//   }
  
  
-  const getFromLocalStorageHandler =() => {
-   let valueAsString = localStorage.getItem('counterKey')
-   if (valueAsString) {
-     let newValue = JSON.parse(valueAsString)
-     setValue(newValue)
-   }
+//   const getFromLocalStorageHandler =() => {
+//    let valueAsString = localStorage.getItem('counterKey')
+//    if (valueAsString) {
+//      let newValue = JSON.parse(valueAsString)
+//      setValue(newValue)
+//    }
   
-    }
+//     }
    
- const clearLocalStorageHandler = () => {
- localStorage.clear()
- setValue(0)
- }
- const removeLocalStorageHandler = () => {
-  localStorage.removeItem('counterKey +1')
- }
+//  const clearLocalStorageHandler = () => {
+//  localStorage.clear()
+//  setValue(0)
+//  }
+//  const removeLocalStorageHandler = () => {
+//   localStorage.removeItem('counterKey +1')
+//  }
 
   return (
 <div className="Conteiner">
       <h1>{value}</h1>
       <button onClick={incHandler}> inc</button>
-      <button onClick={setLocalStorageHandler}> setLocalStorage</button>
-      <button onClick={getFromLocalStorageHandler}> getFromLocalStorage</button>
-      <button onClick={clearLocalStorageHandler}> clearLocalStorage</button>
-      <button onClick={removeLocalStorageHandler}> remoweLocalStorage</button>
+      {/* <button onClick={setLocalStorageHandler}> setLocalStorage</button> */}
+      {/* <button onClick={getFromLocalStorageHandler}> getFromLocalStorage</button> */}
+      {/* <button onClick={clearLocalStorageHandler}> clearLocalStorage</button>
+      <button onClick={removeLocalStorageHandler}> remoweLocalStorage</button> */}
       </div>
   )
 }
